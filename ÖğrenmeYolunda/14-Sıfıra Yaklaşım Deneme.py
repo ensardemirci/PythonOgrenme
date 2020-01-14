25f49e55a34cdff581eafc0d758153667fb40718
Ensar Demirci <ensardemirci93@gmail.com>
2020-01-14 16:59:49 +0300

    Düzeltme

--- ÖğrenmeYolunda/14-Sıfıra Yaklaşım Deneme.py
+++ ÖğrenmeYolunda/14-Sıfıra Yaklaşım Deneme.py
@@ -6,20 +6,22 @@ def f(i):
         i=i-1
     return c
 
+
 def sinus(x):
     k = -1
     u = -3
     t= 0
     while k <= 80:
-        u=u+4
-        k=k+4
-        b = (x**(u))/ f(u)
-        c = -((x**(k))/ f(k))
+        u = u+4
+        k = k+4
+        b = (x ** u) / f(u)
+        c = -((x ** k) / f(k))
         t=b+c+t
         print(t)
     return t
 
-pi=3.141592653589793
+
+pi = 3.141592653589793
 print(sinus(pi/2))
 
 
