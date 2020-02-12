334ec5508d5865b0eb1680f09e39327398b22a98
Ensar Demirci <ensardemirci93@gmail.com>
2020-02-12 15:30:12 +0300

    Datetime Problemi

--- KendiProjelerim/BDO Worker List.py
+++ KendiProjelerim/BDO Worker List.py
@@ -1,18 +1,45 @@
 import sqlite3
-
+from columnar import columnar
+from KendiProjelerim import resetdb,createdb
 
 conn = sqlite3.connect('D:/Ensar Belgeleri/PythonOgrenme/KendiProjelerim/db/bdo.db')
 cur = conn.cursor()
 
-"""cur.execute(''' CREATE TABLE WorkerList(
+"""
+cur.execute(''' CREATE TABLE WorkerList(
 worker_no INTEGER PRIMARY KEY AUTOINCREMENT,
 city_name VARCHAR(50),
 work_place VARCHAR(50),
 product VARCHAR(50),
 working_count INTEGER,
 not_working_count INTEGER,
-total_count INTEGER ) ''') """
+total_count INTEGER ) ''') 
+"""
+def deldb():
+    resetdb.resetdb()
+    createdb.createtable()
 
+def mainmenu():
+    print('\nAna Menü')
+    first = input('\t İşçi [E]kle \t Liste [G]öster \t İşçi [S]il \t Kapat [K]aydet \n --->').strip()
+
+    if first.lower() == 'e':
+        addworker()
+        mainmenu()
+
+    elif first.lower() == 'g':
+        showlist()
+        mainmenu()
+
+    elif first.lower() == 's':
+        deleteworker()
+        mainmenu()
+    elif first.lower() == 'k':
+        conn.close()
+    elif first.lower() == 'r':
+        conn.close()
+        deldb()
+        mainmenu()
 
 def addworker():
     i_city_name = input('Şehir ismini giriniz: ').strip()
@@ -32,19 +59,23 @@ def addworker():
 Çalışan işçi sayısı: {3}
 Boşta işçi sayısı: {4}
 Toplam işçi sayısı: {5}'''.format(i_city_name, i_work_place, i_product, i_working_count, i_not_working_count, i_total_count))
-    last = input('Bilgiler doğru ise kaydetmek için E tekrar girmek için H yazınız').strip()
+    last = input('Bilgiler doğru ise kaydetmek için E tekrar girmek için H yazınız: \n --->').strip()
     if last.lower() == 'e':
         conn.commit()
     else:
-        addworker()
+        mainmenu()
 
 def showlist():
-    list = cur.execute(' SELECT city_name, work_place, product, working_count, not_working_count, total_count FROM WorkerList').fetchall()
-    print('''Şehir İsmi \t Çalışma Alanı \t\t Çıkan Ürün \t Çalışan İşçi Sayısı \t Boşta İşçi Sayısı \t Toplam İşçi Sayısı''')
-    print('''========== \t ============= \t\t ========== \t =================== \t ================= \t ==================''')
-    for row in list:
-        print('{0[0]} \t\t {0[1]} \t {0[2]} \t\t\t\t {0[3]} \t\t\t\t\t\t {0[4]} \t\t\t\t\t {0[5]}'.format(row))
-
+    list_u = list(cur.execute(' SELECT * FROM WorkerList').fetchall())
+    list_n = []
+    for row in list_u:
+        list_n.append([row[0],row[1],row[2],row[3],row[4],row[5],row[6]])
+    header = [' KAYIT NO ',' ŞEHİR İSMİ ',' ÇALIŞMA ALANI ', ' ÇIKAN ÜRÜN ', ' ÇALIŞAN İŞÇİ SAYISI ', ' BOŞTA İŞÇİ SAYISI ',' TOPLAM İŞÇİ SAYISI ']
+    try:
+        table = columnar(list_n, header, no_borders=False, terminal_width=500, max_column_width=20, wrap_max=1, justify='c')
+        print(table)
+    except IndexError:
+        print('Liste Boş')
 
 def deleteworker():
     list = cur.execute(' SELECT * FROM WorkerList').fetchall()
@@ -63,27 +94,14 @@ def deleteworker():
     print('''Kayıt No \t Şehir İsmi \t Çalışma Alanı \t Çıkan Ürün \t Çalışan İşçi Sayısı \t Boşta İşçi Sayısı \t Toplam İşçi Sayısı''')
     print('''======== \t ========== \t ============= \t ========== \t =================== \t ================= \t ==================''')
     for row in list:
-        print('{0[0]} \t\t {0[1]} \t {0[2]} \t\t\t\t {0[3]} \t\t\t\t\t\t {0[4]} \t\t\t\t\t {0[5]} \t\t\t\t\t {0[6]}'.format(row))
+        print('\t{0[0]} \t\t {0[1]} \t\t\t {0[2]} \t {0[3]} \t\t\t\t {0[4]} \t\t\t\t\t\t {0[5]} \t\t\t\t\t {0[6]}'.format(row))
 
     last = input('Kaydetmek için E basınız.')
 
     if last.lower() == 'e':
         conn.commit()
     else:
+        mainmenu()
 
-print('Ana Menü')
-first = ''
-while first.lower() == '' or 'e':
-    first = input('\t İşçi [E]kle \t Liste [G]öster \t İşçi [S]il \n --->')
-    if first.lower() == 'e':
-        addworker()
-        conn.close()
-    elif first.lower() == 'g':
-        showlist()
-        conn.close()
-        break
-    elif first.lower() == 's':
-        deleteworker()
-        conn.close()
-        break
+mainmenu()
 
--- KendiProjelerim/resetdb.py
+++ KendiProjelerim/resetdb.py
@@ -1,12 +1,16 @@
-import os
-import sqlite3
+import os, sqlite3, shutil
+from datetime import datetime,date
+
 
 
 def resetdb():
     last = input('Tüm bilgiler sıfırlanacaktır yedek alınsın mı ? \n [Y]edekle ve Sil \t [S]ıfırla \t [V]azgeç \n ---> ').strip()
     if last.lower() == 's':
+        target = 'D:/Ensar Belgeleri/PythonOgrenme/KendiProjelerim/db/bdo.db'
+        dest = 'D:/Ensar Belgeleri/PythonOgrenme/KendiProjelerim/db/Backup/'
+        shutil.copy(target,'{0}{1}{2}{3}'.format(dest,'bdo',,'.db'))
         open = 'D:/Ensar Belgeleri/PythonOgrenme/KendiProjelerim/db'
         os.chdir(open)
         os.remove('bdo.db')
-        print(os.getcwd())
+        print('{0}\ bdo.db dosyası Backup klasörüne yedeklendi ve sıfırlandı.'.format(os.getcwd()))
 
