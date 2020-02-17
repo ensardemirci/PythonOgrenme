4f9a86889bed936c21b049530142c4814050a837
Ensar Demirci <ensardemirci93@gmail.com>
2020-02-17 15:33:28 +0300

    Revert "EndOftheMeeting"
    This reverts commit e0dc1e4c57d5cbf3e2883d745082ac6078696b72.

--- ÖğrenmeYolunda/FişeklerOrtak/2-Dosya Oluşturma.py
+++
@@ -1,6 +0,0 @@
-
-girdi = input('Dosya ismi giriniz')
-girdi2 = input('Dosya uzantı giriniz')
-
-yol = open('D:/Ensar Belgeleri/PythonOgrenme/ÖğrenmeYolunda/FişeklerOrtak/{0}.{1}'.format(girdi,girdi2),'w+')
-
