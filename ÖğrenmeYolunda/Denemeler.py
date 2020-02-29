4b5f6beda2ebf1c34a5f93f20cb8ec1c7cd35c70
Ensar Demirci <ensardemirci93@gmail.com>
2020-02-29 15:45:42 +0300

    Update Denemeler.py

--- ÖğrenmeYolunda/Denemeler.py
+++ ÖğrenmeYolunda/Denemeler.py
@@ -4,39 +4,73 @@ from PyQt5.QtWidgets import *
 class FirstProgram(QDialog):
     def __init__(self, parent=None):
         super(FirstProgram, self).__init__(parent)
-        self.punto = 3
-        self.label = QLabel('<font  color = "blue" size = "+3" face = "Times New Roman" > Biçimlendirilebilir Metin </font>')
-
         box = QGridLayout()
 
-        self.spinbox = QSpinBox()
-        self.spinbox.setRange(0,4)
-        self.spinbox.setValue(self.punto)
+        waylabel = QLabel('Gideceğiniz Yol (KM): ')
+        self.way = QLineEdit()
+        self.way.setInputMask('0000000000')
+        box.addWidget(waylabel,0,0)
+        box.addWidget(self.way,0,1)
 
-        box.addWidget(self.spinbox,0,2)
+        gasprlabel = QLabel('Yakıtın Litre Fiyatı: ')
+        self.gaspr = QLineEdit()
+        self.gaspr.setInputMask('0.00')
+        box.addWidget(gasprlabel,1,0)
+        box.addWidget(self.gaspr,1,1)
 
-        self.combobox = QComboBox()
-        self.combobox.addItem("Comic Sans MS")
-        self.combobox.addItem('Times New Roman')
-        self.combobox.addItem('Verdana')
-        self.combobox.addItem('Arial')
+        gasconslabel = QLabel('100 KM de Tüketilen Yakıt: ')
+        self.gascons = QLineEdit()
+        self.gascons.setInputMask('00.0')
+        box.addWidget(gasconslabel,2,0)
+        box.addWidget(self.gascons,2,1)
 
+        self.total = QLabel('<font  color = "blue" size = "+2"> Sonuç </font>')
+        totallabel = QLabel('Toplam Tutar: ')
 
-        box.addWidget(self.combobox,1,2)
+        box.addWidget(totallabel,3,0)
+        box.addWidget(self.total,3,1)
 
-        box.addWidget(self.label, 0, 0, 2, 1)
-
-        self.spinbox.valueChanged.connect(self.changeFP)
-        self.combobox.currentIndexChanged.connect(self.changeFP)
+        button = QPushButton('Hesapla')
+        button.clicked.connect(self.calculate)
+        box.addWidget(button,4,0,1,2)
 
         self.setLayout(box)
-        self.setWindowTitle('PyQt Combo & Spin Box')
-        self.resize(600,200)
+        self.setWindowTitle('Yakıt Ücret Hesaplayıcı')
+        self.resize(300,200)
         self.move(550,400)
-    def changeFP(self):
-            self.label.setText('<font  color = "blue" size = "+{1}" face = "{0}"> Biçimlendirilebilir Metin </font>'.format(self.combobox.currentText(),self.spinbox.value()))
-            print('Font: ',self.combobox.currentText())
-            print('Punto: ',self.spinbox.value())
+
+    def calculate(self):
+        iway = 0
+        igascons = 0
+        igaspr = 0
+        itotal = 0
+        try:
+            iway = int(self.way.text())
+        except:
+            pass
+
+        try:
+            igaspr = float(self.gaspr.text())
+        except:
+            pass
+
+        try:
+            igascons = float(self.gascons.text())
+        except:
+            pass
+        if not iway:
+            self.total.setText('<font  color = "red" size = "+2"> KM Giriniz  </font>')
+            self.way.setFocus()
+        elif not igaspr:
+            self.total.setText('<font  color = "red" size = "+2"> Litre Fiyatı Giriniz  </font>')
+            self.gaspr.setFocus()
+        elif not igascons:
+            self.total.setText('<font  color = "red" size = "+2"> Tüketimi Giriniz  </font>')
+            self.gascons.setFocus()
+        else:
+            itotal = igaspr*(iway*igascons)/100
+
+            self.total.setText('<font  color = "green" size = "+2"> {0} ₺ </font>'.format(itotal))
 
 app = QApplication([])
 window = FirstProgram()
